ca2090ac83e95ccb765280a2696a25db42d0cc9d
codekeep18feb <codekeep18feb@gmail.com>
2024-11-12 20:17:35 +0530

    Deploy static files to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    assetPrefix: '/chathead-landing-next/',  // Set directly for GitHub Pages
+    images: { unoptimized: true },
+    env: {
+      NEXT_PUBLIC_API_URL: 'https://api.example.com', // Use production URL directly
+    },
+  };
+  
+  export default nextConfig;
+  
