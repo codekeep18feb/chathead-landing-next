b323006a6d296be6ed2ecfa9e8fb860e4a438c1a
Deepak Singh <codekeep.18feb@gmail.com>
2024-11-17 19:40:23 +0530

    Deploy to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,9 @@
 const isProd = process.env.NODE_ENV === 'production';
 
-
-console.log(`Building for environment: ${process.env.NODE_ENV}`);
-console.log(`Asset prefix set to: ${isProd ? '/chathead-landing-next/' : ''}`);
-
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  assetPrefix: isProd ? '/chathead-landing-next/' : '', // Only set for production
+  assetPrefix: isProd ? '/chathead-landing-next/' : '',  // Only set for production
+  basePath: isProd ? '/chathead-landing-next' : '', // Add basePath to handle the subdirectory
   images: { unoptimized: true },
   env: {
     NEXT_PUBLIC_API_URL: 'https://api.example.com',
