60ce826a3b36d69e68ebee76ba63380192a57fd3
Deepak Singh <codekeep.18feb@gmail.com>
2024-11-17 18:06:32 +0530

    fixed missing css

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
-// next.config.mjs
+const isProd = process.env.NODE_ENV === 'production';
 
+console.log("canwe log some here?")
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    assetPrefix: '/chathead-landing-next/',  // Set directly for GitHub Pages
-    images: { unoptimized: true },
-    env: {
-      NEXT_PUBLIC_API_URL: 'https://api.example.com', // Use production URL directly
-    },
-  };
-  
-  export default nextConfig;
-  
+  output: 'export',
+  assetPrefix: isProd ? '/chathead-landing-next/' : '', // Only set for production
+  images: { unoptimized: true },
+  env: {
+    NEXT_PUBLIC_API_URL: 'https://api.example.com',
+  },
+};
+
+export default nextConfig;
