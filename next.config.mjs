433c2f8c0d74365cebc86a5c5490a4ebc92106be
Deepak Singh <codekeep.18feb@gmail.com>
2024-11-17 21:27:45 +0530

    updated few things to see if that changes will go through

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  output: 'export',
-  assetPrefix: isProd ? '/chathead-landing-next/' : '',  // Only set for production
-  basePath: isProd ? '/chathead-landing-next' : '', // Add basePath to handle the subdirectory
-  images: { unoptimized: true },
-  env: {
-    NEXT_PUBLIC_API_URL: 'https://api.example.com',
-  },
+    output: "export",
+    assetPrefix: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : "",
+    basePath: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : "",
+    images: { unoptimized: true },
+    env: {
+        NEXT_PUBLIC_BASE_PATH: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : "",
+        NEXT_PUBLIC_API_URL: "https://api.example.com",
+    },
 };
 
 export default nextConfig;
--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,28 +1,42 @@
-// src/app/components/Navbar.js
 "use client"; // This line must be at the top
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false); // Manage menu state
 
     const toggleMenu = () => {
-        setMenuOpen(prev => !prev); // Toggle the state
+        setMenuOpen((prev) => !prev); // Toggle the state
     };
 
+    // Use the basePath for images
+    const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+    console.log("base pathDFD",basePath)
     return (
         <nav className="navbar">
             <div className="logo">
-                <img src="/Asset/logo.png" alt="quickChat" />
+                <img src={`${basePath}/Asset/logo.png`} alt="quickChat" />
                 <h2>quickChat</h2>
             </div>
-            <div className="burger-icon" onClick={toggleMenu}>&#9776;</div>
-            <ul className={`nav-links ${menuOpen ? 'active' : ''}`} id="nav-links">
-                <li><a href="#platform" className="link">Platform</a></li>
-                <li><a href="#solutions" className="link">Solutions</a></li>
-                <li><a href="#developer" className="link">Developers</a></li>
-                <li><a href="#resource" className="link">Resources</a></li>
-                <li><a href="#pricing" className="link">Pricing</a></li>
+            <div className="burger-icon" onClick={toggleMenu}>
+                &#9776;
+            </div>
+            <ul className={`nav-links ${menuOpen ? "active" : ""}`} id="nav-links">
+                <li>
+                    <a href="#platform" className="link">Platform</a>
+                </li>
+                <li>
+                    <a href="#solutions" className="link">Solutions</a>
+                </li>
+                <li>
+                    <a href="#developer" className="link">Developers</a>
+                </li>
+                <li>
+                    <a href="#resource" className="link">Resources</a>
+                </li>
+                <li>
+                    <a href="#pricing" className="link">Pricing</a>
+                </li>
                 <div className="auth-buttons">
                     <li>
                         <a href="http://122.160.157.99:3101/newlogin">
