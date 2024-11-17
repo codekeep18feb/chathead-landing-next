4f2909e4813e3f2dfde514f8119c842640c870de
Deepak Singh <codekeep.18feb@gmail.com>
2024-11-17 21:55:58 +0530

    updated images on local

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,11 @@ const isProd = process.env.NODE_ENV === "production";
 console.log("building with process.env.NEXT_PUBLIC_BASE_PATH",process.env.NEXT_PUBLIC_BASE_PATH,process.env.NODE_ENV)
 const nextConfig = {
     output: "export",
-    assetPrefix: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : "",
-    basePath: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : "",
+    assetPrefix: isProd ? "/chathead-landing-next" : "",
+    basePath: isProd ? "/chathead-landing-next" : "",
     images: { unoptimized: true },
     env: {
-        NEXT_PUBLIC_BASE_PATH: "/chathead-landing-next",
+        NEXT_PUBLIC_BASE_PATH: isProd ? "/chathead-landing-next" : "",
         NEXT_PUBLIC_API_URL: "https://api.example.com",
     },
 };
