265aa516490e96e7f68f4b10bc07c6e61279608a
codekeep18feb <codekeep18feb@gmail.com>
2025-03-24 14:52:57 +0530

    created customer stories section

--- src/app/components/solutions_items/matrimony/carousel.js
+++ src/app/components/solutions_items/matrimony/carousel.js
@@ -5,28 +5,28 @@ import carouselSty from "./carousel.module.css";
 const slides = [
   {
     image:
-      "https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000",
+      "https://images.unsplash.com/photo-1597157639073-69284dc0fdaf?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW5kaWFuJTIwd2VkZGluZ3xlbnwwfHwwfHx8MA%3D%3D",
     alt: "Wedding 1",
     text: "“Our existing chat system struggled with the growing user base, leading to performance hiccups. We had to shift, and with AddChat, we improved our matching algorithm, increasing match rates by 20%.”",
-    logo: "/shaadi-logo.png",
+    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFcfko4VAZJJGM4OHDMC4AciePcXTTLGoy6w&s",
     author: "Reetu Singh",
     designation: "Product Manager",
   },
   {
     image:
-      "https://static.vecteezy.com/system/resources/thumbnails/046/463/053/small_2x/tiger-face-running-to-hunt-down-prey-in-winter-on-snow-snowfall-climate-blurred-background-photo.jpg",
+      "https://www.candidshutters.com/maintenance/wp-content/uploads/2024/06/Best-wedding-photographers-India-Top-5-destination-wedding-photographers-Indian-weddings-2.jpg",
     alt: "Wedding 2",
     text: "“A seamless integration with AddChat allowed us to boost user engagement by 30%, creating meaningful connections.”",
-    logo: "/tinder-logo.png",
+    logo: "https://d3lzcn6mbbadaf.cloudfront.net/media/details/ANI-20230224091946.jpg",
     author: "Arjun Mehra",
     designation: "CTO, Matrimony App",
   },
   {
     image:
-      "https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/1015f/MainBefore.jpg",
+      "https://cdn0.weddingwire.in/vendor/9778/3_2/960/jpg/wedding-photographers-shaadigram-art-couple-shot-3_15_449778-168613815393146.jpeg",
     alt: "Wedding 3",
     text: "“AddChat’s real-time chat improved our platform significantly. Users now connect effortlessly, and our retention rates have skyrocketed.”",
-    logo: "/bumble-logo.png",
+    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcST7OFdEEZduebRh58MO-Up4qkbFzsBPdNrtA&s",
     author: "Priya Sharma",
     designation: "Head of Product",
   },
@@ -45,25 +45,34 @@ const Carousel = () => {
 
   return (
     <div className={carouselSty.carousel}>
+      <h1>10 years and 50,000+ customer Stories</h1>
+      <p>Customer Stories</p>
+
       <div className={carouselSty.arrowBtn}>
         <button className={carouselSty.leftArrow} onClick={prevSlide}>
           <FaChevronLeft size={18} />
         </button>
         <button className={carouselSty.rightArrow} onClick={nextSlide}>
-          <FaChevronRight  size={18} />
+          <FaChevronRight size={18} />
         </button>
       </div>
 
       <div
         className={carouselSty.slideContainer}
-        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+        style={{ transform: `translateX(calc(-${currentIndex * 95}% - 40px))` }}
       >
         {slides.map((slide, index) => (
-          <div key={index} className={carouselSty.slide}>
+          <div
+            key={index}
+            className={`${carouselSty.slide} ${
+              index === slides.length - 1 ? carouselSty.lastSlide : ""
+            }`}
+          >
             <img
               src={slide.image}
               alt={slide.alt}
               className={carouselSty.image}
+              onClick={nextSlide}
             />
             <div className={carouselSty.overlay}>
               <div className={carouselSty.testimonial}>
@@ -72,12 +81,20 @@ const Carousel = () => {
                   alt="Company Logo"
                   className={carouselSty.logo}
                 />
-                <p className={carouselSty.text}>{slide.text}</p>
-                <div className={carouselSty.author}>
-                  <span className={carouselSty.name}>{slide.author}</span> —{" "}
-                  <span className={carouselSty.role}>{slide.designation}</span>
+                <div className={carouselSty.text_content}>
+                  <p className={carouselSty.text}>{slide.text}</p>
+                  <div className={carouselSty.author}>
+                    <div>
+                      <span className={carouselSty.name}>{slide.author}</span> —{" "}
+                      <span className={carouselSty.role}>
+                        {slide.designation}
+                      </span>
+                    </div>
+                    <button className={carouselSty.readMore}>
+                      Read more →
+                    </button>
+                  </div>
                 </div>
-                <button className={carouselSty.readMore}>Read more →</button>
               </div>
             </div>
           </div>
