95fdb3a3c2c21200c04be71ead3018a9eb065582
reetuTheCoder <codewithreetu@gmail.com>
2025-09-10 17:57:11 +0530

    make modal list item path with parent li in the platform

---
+++ src/app/platform/[feature]/page.js
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactionsPage from "@/app/components/platform_items/reactions/page";
+import DeliveryReports from "@/app/components/platform_items/delivery-reports/page";
+import LiveStatus from "@/app/components/platform_items/live-status/page";
+import EmailNotifications from "@/app/components/platform_items/email-notifications/page";
+
+
+export async function generateStaticParams() {
+  // List all your features here (lowercase, URL-friendly)
+  const features = [
+    "reactions",
+    "delivery-reports",
+    "live-status",
+    "email-notifications",
+    // ...
+  ];
+
+  return features.map((feature) => ({ feature }));
+}
+
+export default async function FeaturePage  ({ params }) {
+   const resolvedParams = await Promise.resolve(params);
+  const { feature } = resolvedParams;
+
+  // Map URL param → React component
+  const featureComponents = {
+    "reactions": <ReactionsPage />,
+    "delivery-reports": <DeliveryReports/>,
+    "live-status": <LiveStatus/>,
+    "email-notifications": <EmailNotifications/>,
+
+
+    // ...
+  };
+
+  const Component = featureComponents[feature];
+
+  if (!Component) return <h1>Feature not found</h1>;
+
+  return Component;
+};
+
