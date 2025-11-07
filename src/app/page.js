7572ef72baa9c5696632bf3d1bd9188b919fda82
reetuTheCoder <codewithreetu@gmail.com>
2025-11-07 16:33:19 +0530

    fix image css

--- src/app/page.js
+++ src/app/page.js
@@ -37,7 +37,7 @@ export default function Home() {
       <br></br>
 
         <br></br>
-      <SolveSectioncopy />
+      {/* <SolveSectioncopy /> */}
       <WhatWeOfferSection/>
       <MagicChatOverview />
       {/* <HowSection /> */}
