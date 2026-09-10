461fc4e912e1109da6fe99639b174ea6fff8c205
codekeep18feb <codekeep18feb@gmail.com>
2026-09-10 19:04:39 +0530

    added background on the code

--- src/app/documentation/payload/versions/V2/clientIntegration.js
+++ src/app/documentation/payload/versions/V2/clientIntegration.js
@@ -312,12 +312,12 @@ export const clientIntegration = [
                         tag_type: "code_with_copy",
                         code: `<script>
   window.__sageionSetup = (async function () {
-    await window.magicchat_io.setUp(
+    [[[await window.magicchat_io.setUp(
       "your_app_name",
       "YOUR_API_KEY",
       "US",
       "sageion-chat-root"
-    );
+    );]]]
 
     // Resolve the initialize() payload based on login state.
     const token = localStorage.getItem("token");
@@ -388,12 +388,13 @@ export const clientIntegration = [
                         tag_type: "code_with_copy",
                         code: `<script>
   window.__sageionSetup = (async function () {
-    await window.magicchat_io.setUp(
+    [[[await window.magicchat_io.setUp(
       "your_app_name",
       "YOUR_API_KEY",
       "US",
       "sageion-chat-root"
-    );
+    );]]]
+
     const token = localStorage.getItem("token");
     if (!token) return {};
     try {
